d8e71c3b8efef10d6633eff16a29f642f5cf879c
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-09-16 14:20:28 -0500

    add files SpringJPA

--- src/main/java/com/practice/App.java
+++ src/main/java/com/practice/App.java
@@ -7,6 +7,10 @@ import com.practice.repository.EmployeeRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -40,5 +44,17 @@ public class App {
         );
         employeeRepo.saveAll(employees);
     }
-
+@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 }
