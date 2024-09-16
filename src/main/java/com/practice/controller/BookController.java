ac0841fd389f4b2101e728016d17c94e2d5e3c49
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-09-16 01:29:01 -0500

    add files SpringJPA

--- src/main/java/com/practice/controller/BookController.java
+++ src/main/java/com/practice/controller/BookController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@RequestMapping("/api/books")
 @RestController()
 public class BookController {
     //Atributos
@@ -19,24 +20,28 @@ public class BookController {
         this.bookRepository = bookRepository;
     }
 
-    @GetMapping("/api/books")
+    @GetMapping()
     public List<Book> fillAdd() {
         return bookRepository.findAll();
     }
-    @GetMapping("/api/books/{id}")
-    public Book fillById(@PathVariable Long id){
+
+    @GetMapping("/{id}")
+    public Book fillById(@PathVariable Long id) {
         Optional<Book> book = bookRepository.findById(id);
         return book.get();
     }
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping("api/books")
-    public Book create( @RequestBody Book book){
-        return bookRepository.save(book);
+    public ResponseEntity<Book> create(@RequestBody Book book) {
+        Book resp = bookRepository.save(book);
+        return ResponseEntity.ok(resp);
     }
-    @DeleteMapping("/api/books/{id}")
-    public ResponseEntity<Book> deleteById(@PathVariable Long id){
-      bookRepository.deleteById(id);
-       return ResponseEntity.noContent().build();
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Book> deleteById(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 
